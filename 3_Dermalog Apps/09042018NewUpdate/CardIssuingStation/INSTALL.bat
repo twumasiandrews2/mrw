b9c18059b1b3b3285c96d3e102870e8947240606
ims2018 <39301005+ims2018@users.noreply.github.com>
2018-05-18 07:29:51 +0000

    Update INSTALL.bat
    Added temp fix for card issuing software ip

--- 3_Dermalog Apps/09042018NewUpdate/CardIssuingStation/INSTALL.bat
+++ 3_Dermalog Apps/09042018NewUpdate/CardIssuingStation/INSTALL.bat
@@ -3,3 +3,5 @@
 Title Installing Card Reader Service
 REM TO REMOVE FOLDER SPACES
 msiexec -i %~dp0CardReaderServiceSetup_PROD.msi /qb!-
+rem copyConfig File TO demalog to install folder
+copy /y "%~dp0CardIssuingStation.exe.config"  "%PROGRAMFILES(X86)%\DERMALOG\Dermalog Card Issuing Station\CardIssuingStation.exe.config"
