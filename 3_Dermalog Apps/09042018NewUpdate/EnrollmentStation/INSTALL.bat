6874cc6c5c7e33230532d607fbd322b34ef24458
ims2018 <39301005+ims2018@users.noreply.github.com>
2018-05-18 06:41:32 +0000

    Update INSTALL.bat

--- 3_Dermalog Apps/09042018NewUpdate/EnrollmentStation/INSTALL.bat
+++ 3_Dermalog Apps/09042018NewUpdate/EnrollmentStation/INSTALL.bat
@@ -1,4 +1,4 @@
 @echo off
 Title Installing Enrollment Software
 REM TO REMOVE FOLDER SPACES
-msiexec -i %~dp0EnrollmentStationSetup-MRW2.msi /l*v log.txt BACKENDIP=10.120.200.241 /qb!-
+msiexec -i %~dp0EnrollmentStationSetup-MRW2.msi BACKENDIP=10.120.200.241 /qb!-
